dd80fae8856a5ab92060a06d169afb9ec7e348c1
manuel.aguilar2 <manuel.aguilar2@hccs.edu>
2019-07-30 15:15:22 -0500

    Adding seeders for Faker and adding migration file to create new column into slip_session table

---
+++ database/seeds/SlipSessionSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\SlipSession;
+
+class SlipSessionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = array(
+            array()
+
+        );
+    }
+}
